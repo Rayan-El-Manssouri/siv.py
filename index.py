98406d0ae7dd16275533d415663101ca038710c3
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-02-07 14:26:19 +0100

    Ajout de la premiére image

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import PhotoImage
 
 root = tk.Tk()
 root.title("Siv - Welcom")
@@ -8,23 +9,25 @@ root.geometry("1280x720")
 def button_click():
     print("Button was clicked")
 
-GestionPersonne = tk.Button(root, text="Gestion personne", command=button_click)
-GestionPersonne.pack()
 
-GestionVehicule = tk.Button(root, text="Gestion véhicule!", command=button_click)
-GestionVehicule.pack()
-
-GestionOperation = tk.Button(root, text="Gestion opération", command=button_click)
-GestionOperation.pack()
 
 # définir la couleur de la fenêtre
 root.configure(bg='white')
 
+# Charger l'image
+image = PhotoImage(file="./src/profil.png")
+
+
+# Créer un canvas pour l'image
+canvas = tk.Canvas(root, bg='white')
+canvas.pack(fill='both', expand=True)
+
+# Dessiner l'image sur le canvas
+canvas.create_image(0, 0, image=image, anchor='nw')
+
+
 # ajouter une barre de menu
 menu_bar = tk.Menu(root, bg='black', fg='white')
 root.config(menu=menu_bar)
-
-
-
 
 root.mainloop()
