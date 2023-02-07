15bb5d868b497b7a4860c502661e55410fb3cb76
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-02-07 14:01:54 +0100

    Ajout d'une photo de profil

--- index.py
+++ index.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Clean GUI")
-root.iconbitmap("logo.png")
+root.title("Siv - Welcom")
+root.geometry("1280x720")
+
 
 def button_click():
     print("Button was clicked")
 
-button = tk.Button(root, text="Click Me!", command=button_click)
-button.pack()
+GestionPersonne = tk.Button(root, text="Gestion personne", command=button_click)
+GestionPersonne.pack()
+
+GestionVehicule = tk.Button(root, text="Gestion véhicule!", command=button_click)
+GestionVehicule.pack()
+
+GestionOperation = tk.Button(root, text="Gestion opération", command=button_click)
+GestionOperation.pack()
+
+# définir la couleur de la fenêtre
+root.configure(bg='white')
+
+# ajouter une barre de menu
+menu_bar = tk.Menu(root, bg='black', fg='white')
+root.config(menu=menu_bar)
+
+
+
 
 root.mainloop()
