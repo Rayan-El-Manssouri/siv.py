7552b1dcb060bbb8c6f6e77f2caee30df984bf93
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-02-07 11:47:00 +0100

    Ico : mise a jour de l'icon

--- index.py
+++ index.py
@@ -1,18 +1,13 @@
 import tkinter as tk
-from tkinter import PhotoImage
+
+root = tk.Tk()
+root.title("Clean GUI")
+root.iconbitmap("logo.png")
 
 def button_click():
     print("Button was clicked")
 
-root = tk.Tk()
-root.title("Clean GUI")
-root.geometry("400x200")
-
-icon = PhotoImage(file="logo.png")
-root.iconphoto(True, icon)
-root.wm_iconbitmap("logo.png")
-
-button = tk.Button(root, text="Click Me!", command=button_click, bg="lightblue", fg="white", font=("Helvetica", 16))
-button.pack(pady=10)
+button = tk.Button(root, text="Click Me!", command=button_click)
+button.pack()
 
 root.mainloop()
