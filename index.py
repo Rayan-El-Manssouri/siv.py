4b4ef677c2bc1c4b21dc66e53ea34376423c9560
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-02-07 14:42:31 +0100

    Ajout d'une image de profil

--- index.py
+++ index.py
@@ -5,11 +5,8 @@ root = tk.Tk()
 root.title("Siv - Welcom")
 root.geometry("1280x720")
 
-
 def button_click():
     print("Button was clicked")
-
-
 
 # définir la couleur de la fenêtre
 root.configure(bg='white')
@@ -17,13 +14,17 @@ root.configure(bg='white')
 # Charger l'image
 image = PhotoImage(file="./src/profil.png")
 
+# Redimensionner l'image de profil
+new_width = 120
+new_height = 120
+image = image.subsample(image.width() // new_width, image.height() // new_height)
 
 # Créer un canvas pour l'image
 canvas = tk.Canvas(root, bg='white')
 canvas.pack(fill='both', expand=True)
 
 # Dessiner l'image sur le canvas
-canvas.create_image(0, 0, image=image, anchor='nw')
+canvas.create_image(1250, 10, image=image, anchor='ne')
 
 
 # ajouter une barre de menu
